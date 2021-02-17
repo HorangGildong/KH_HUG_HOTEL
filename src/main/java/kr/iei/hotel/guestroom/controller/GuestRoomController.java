c3cc4cb9364ecd1f175bb078742049af3c4f883b
choijuyoung <wndud0159@naver.com>
2021-02-17 10:19:39 +0900

    JYP

--- src/main/java/kr/iei/hotel/guestroom/controller/GuestRoomController.java
+++ src/main/java/kr/iei/hotel/guestroom/controller/GuestRoomController.java
@@ -1,9 +1,11 @@
 package kr.iei.hotel.guestroom.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +15,8 @@ import kr.iei.hotel.guestroom.vo.GuestRoomVO;
 
 @Controller
 public class GuestRoomController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GuestRoomController.class);
 	
 	@Autowired
 	GuestRoomService guestRoomService;
@@ -29,24 +33,42 @@ public class GuestRoomController {
 	}
 	
 	//-------------------------------------Admin--------------------------------------------
-
+	//객실 관리 리스트 뷰
 	@RequestMapping(value = "/guestRoomAdminView", method = RequestMethod.GET)
-	public String guestRoomlistAdmin()throws Exception {
+	public String guestRoomAdminList(Model model)throws Exception {
+		
+		logger.info("객실 관리 리스트");
+		model.addAttribute("list", guestRoomService.guestRoomAdminList());
 		return "/guestroomAdmin/guestRoomAdminView";
+	
 	}
-
+	//객실 정보 추가 뷰
 	@RequestMapping(value = "/guestRoomAddView", method = RequestMethod.GET)
 	public String guestRoomAddView()throws Exception {
+		
+		logger.info("객실 정보 추가 뷰");
 		return "/guestroomAdmin/guestRoomAddView";
-	}
 	
+	}
+	//객실 정보 추가
 	@RequestMapping(value = "/guestRoomInfoInsert", method = RequestMethod.POST)
 	public String guestRoomInfoInsert(GuestRoomVO guestRoomVO)throws Exception {
+		
+		logger.info("객실 정보 추가");
 		guestRoomVO.setGuestRoomImage1(guestRoomVO.getImage1().getOriginalFilename());
 		guestRoomVO.setGuestRoomImage2(guestRoomVO.getImage2().getOriginalFilename());
 		guestRoomVO.setGuestRoomImage3(guestRoomVO.getImage3().getOriginalFilename());
 		guestRoomVO.setGuestRoomImage4(guestRoomVO.getImage4().getOriginalFilename());
 		guestRoomService.guestRoomInfoInsert(guestRoomVO);
-		return "guestRoomAddView";
+		return "redirect:/guestRoomAdminView";
+	}
+	//객실 정보 삭제
+	@RequestMapping(value = "/guestRoomInfoDelete", method = RequestMethod.GET)
+	public String guestRoomInfo(String guestRoomName)throws Exception {
+		
+		logger.info("객실 정보 삭제");
+		guestRoomService.guestRoomInfoDelete(guestRoomName);
+		return "redirect:/guestRoomAdminView";
+	
 	}
 }
--- src/main/java/kr/iei/hotel/guestroom/service/GuestRoomService.java
+++ src/main/java/kr/iei/hotel/guestroom/service/GuestRoomService.java
@@ -2,6 +2,8 @@ package kr.iei.hotel.guestroom.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +16,20 @@ public class GuestRoomService {
 	
 	@Autowired
 	GuestRoomMapper guestRoomMapper;
+	//-------------------------------------User--------------------------------------------
+	//-------------------------------------Admin--------------------------------------------
 	
+	//객실 관리 리스트
+	public List<GuestRoomVO> guestRoomAdminList()throws Exception {
+		return guestRoomMapper.guestRoomAdminList();
+	}
+	//객실 정보 추가
 	public void guestRoomInfoInsert(GuestRoomVO guestRoomVO)throws Exception {
 		guestRoomMapper.guestRoomInfoInsert(guestRoomVO);
+	}
+	//객실 정보 삭제
+	public void guestRoomInfoDelete(String guestRoomName)throws Exception {
+		guestRoomMapper.guestRoomInfoDelete(guestRoomName);
 	}
 	
 
