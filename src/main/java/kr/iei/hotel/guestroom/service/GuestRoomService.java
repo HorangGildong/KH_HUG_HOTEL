d0bffae2617dc68225941a76ae9964f8e00cfbdb
choijuyoung <wndud0159@naver.com>
2021-02-17 16:31:07 +0900

    JYP

--- src/main/java/kr/iei/hotel/guestroom/service/GuestRoomService.java
+++ src/main/java/kr/iei/hotel/guestroom/service/GuestRoomService.java
@@ -31,6 +31,18 @@ public class GuestRoomService {
 	public void guestRoomDelete(String guestRoomName)throws Exception {
 		guestRoomMapper.guestRoomDelete(guestRoomName);
 	}
+	//객실 관리 정보 리스트
+	public List<GuestRoomVO> guestRoomAdminInfoList()throws Exception {
+		return guestRoomMapper.guestRoomAdminInfoList();
+	}
+	//객실 정보 추가
+	public void guestRoomInfoInsert(GuestRoomVO guestRoomVO)throws Exception {
+		guestRoomMapper.guestRoomInfoInsert(guestRoomVO);
+	}
+	//객실 정보 삭제
+	public void guestRoomInfoDelete(int guestRoomNo)throws Exception {
+		guestRoomMapper.guestRoomInfoDelete(guestRoomNo);
+	}
 	
 
 }
