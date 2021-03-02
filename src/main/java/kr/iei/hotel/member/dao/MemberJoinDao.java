1536d60c3bd320bd93a7bf2532a5b8d409c45eff
HorangGildong <horanggildong@gmail.com>
2021-03-03 00:05:51 +0900

    horanggildong_20210303_0005

--- src/main/java/kr/iei/hotel/member/dao/MemberJoinDao.java
+++ src/main/java/kr/iei/hotel/member/dao/MemberJoinDao.java
@@ -1,7 +1,5 @@
 package kr.iei.hotel.member.dao;
 
-import java.sql.Date;
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
